6ace92d60f27aeda1e7643f08c803f1ea00aae39
SJ-KR <djtm99@naver.com>
2020-05-17 19:32:48 -0700

    update Hello const

--- Hello/Hello.go
+++ Hello/Hello.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
+const eng = "Hello"
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+	return fmt.Sprintf("%s, %s", eng, name)
 }
 func main() {
 	fmt.Println(Hello("world"))
