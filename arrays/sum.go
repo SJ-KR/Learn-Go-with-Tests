736ad25d4b118d8ba7e1ae1c42a1dd9bf440bdc6
SJ-KR <djtm99@naver.com>
2020-05-19 02:54:31 -0700

    update SumAll()

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,18 @@
 package arrays
 
-func Sum(nums [5]int) int {
+func Sum(nums []int) int {
 	sum := 0
 	for _, num := range nums {
 		sum += num
 	}
 	return sum
+}
+func SumAll(numbersToSum ...[]int) []int {
+	length := len(numbersToSum)
+	sums := make([]int, length)
+
+	for i := 0; i < length; i++ {
+		sums[i] = Sum(numbersToSum[i])
+	}
+	return sums
 }
