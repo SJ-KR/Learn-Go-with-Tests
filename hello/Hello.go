812a35479138b2d87a22fa1c706e6bb29f5581bb
SJ-KR <djtm99@naver.com>
2020-05-19 01:44:24 -0700

    update Hello test (french)

--- hello/Hello.go
+++ hello/Hello.go
@@ -3,19 +3,26 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
-const englishhello = "Hello"
-const spanishhello = "Hola"
+const french = "French"
+const englishHello = "Hello"
+const spanishHello = "Hola"
+const frenchHello = "Bonjour"
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 	if language == spanish {
-		return fmt.Sprintf("%s, %s", spanishhello, name)
+		return fmt.Sprintf("%s, %s", spanishHello, name)
+	}
+	if language == french {
+		return fmt.Sprintf("%s, %s", frenchHello, name)
 	}
 
-	return fmt.Sprintf("%s, %s", englishhello, name)
+	return fmt.Sprintf("%s, %s", englishHello, name)
 }
 func main() {
-	fmt.Println(Hello("World", "Hello"))
+	fmt.Println(Hello("World", "Spanish"))
+	fmt.Println(Hello("World", "French"))
+	fmt.Println(Hello("World", ""))
 }
