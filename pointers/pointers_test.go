ad2e21ebd55ed9be801765f6d379907e0ad39dd7
SJ-KR <djtm99@naver.com>
2020-05-20 02:19:07 -0700

    update wallet (a new method : Withdraw())

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -1,20 +1,33 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	fmt.Printf("address of balance in test is %v\n", &wallet.balance)
-	want := Bitcoin(10)
+		got := wallet.Balance()
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
 }
