b69b038447a23588b1aa113b3b07a2f8e6f0a51f
SJ-KR <djtm99@naver.com>
2020-05-19 22:01:16 -0700

    update structs (methods)

--- structs/structs.go
+++ structs/structs.go
@@ -1,10 +1,24 @@
 package structs
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
