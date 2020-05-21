dc17ea380ec1acd83444db606dcdb2474a2e9d50
SJ-KR <djtm99@naver.com>
2020-05-21 03:46:18 -0700

    update countdown (time.Sleep())

--- moking/moking.go
+++ moking/moking.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(w io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(w, i)
-		//time.Sleep(time.Second)
 	}
-	fmt.Fprint(w, "Go!")
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(w, finalWord)
 }
 
 func main() {
