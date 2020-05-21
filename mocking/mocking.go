c721bb479e05fd90142b00dbed16769b070c2e66
SJ-KR <djtm99@naver.com>
2020-05-21 09:27:09 -0700

    update countdown (Configurable Sleeper)

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -24,13 +24,15 @@ func (s *SpySleeper) Sleep() {
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+	//time.Sleep(time.Second)
+	time.Sleep(time.Millisecond * 250) // for saving my time
 }
 
 func Countdown(w io.Writer, s Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		s.Sleep()
 		fmt.Fprintln(w, i)
+		//fmt.Println(i)
 	}
 	s.Sleep()
 	fmt.Fprint(w, finalWord)
@@ -50,6 +52,24 @@ func (s *CountdownOperationsSpy) Write(b []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
 }
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type Spytime struct {
+	durationSlept time.Duration
+}
+
+func (s *Spytime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 func main() {
 	sleeper := &DefaultSleeper{}
 	Countdown(os.Stdout, sleeper)
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -4,26 +4,19 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
 
 	t.Run("spy sleep test", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spy := &SpySleeper{}
-
-		Countdown(buffer, spy)
+		Countdown(buffer, &DefaultSleeper{})
 
 		got := buffer.String()
-		want := `3
-2
-1
-Go!`
+		want := "3\n2\n1\nGo!"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
-		}
-		if spy.Calls != 4 {
-			t.Errorf("not enough calls to sleeper, want 4 got %d", spy.Calls)
 		}
 	})
 
@@ -45,5 +38,18 @@ Go!`
 			t.Errorf("wanted calls %v got %v", want, SpySleepPrinter.Calls)
 		}
 	})
+
+}
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &Spytime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 
 }
