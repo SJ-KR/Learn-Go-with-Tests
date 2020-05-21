e56a529093eeb3bca4946d31efc03653486b34e0
SJ-KR <djtm99@naver.com>
2020-05-21 03:44:06 -0700

    update countdown (3 2 1 Go!)

--- moking/moking.go
+++ moking/moking.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Countdown(w io.Writer) {
-	fmt.Fprint(w, "3")
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(w, i)
+		//time.Sleep(time.Second)
+	}
+	fmt.Fprint(w, "Go!")
 }
 
 func main() {
-	//Countdown()
+	Countdown(os.Stdout)
 }
