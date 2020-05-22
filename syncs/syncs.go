89cc459e83360a43e0c4cfb2b66c975c4c1ccb1c
SJ-KR <djtm99@naver.com>
2020-05-22 12:48:09 -0700

    update syncs (using mutex-lock for the concurrency control)

--- syncs/syncs.go
+++ syncs/syncs.go
@@ -1,10 +1,19 @@
 package syncs
 
+import "sync"
+
 type Counter struct {
 	count int
+	sync.Mutex
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
 }
 
 func (c *Counter) Inc() {
+	c.Lock()
+	defer c.Unlock()
 	c.count += 1
 }
 func (c *Counter) Value() int {
--- syncs/syncs_test.go
+++ syncs/syncs_test.go
@@ -1,10 +1,13 @@
 package syncs
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 
 		counter.Inc()
 		counter.Inc()
@@ -12,8 +15,24 @@ func TestCounter(t *testing.T) {
 
 		assertCounter(t, counter, 3)
 	})
+	t.Run("safely concurrently", func(t *testing.T) {
+		want := 1000
+		counter := NewCounter()
+
+		var waitgroup sync.WaitGroup
+		waitgroup.Add(want)
+		for i := 0; i < want; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&waitgroup)
+		}
+		waitgroup.Wait()
+		assertCounter(t, counter, want)
+	})
+
 }
-func assertCounter(t *testing.T, got Counter, want int) {
+func assertCounter(t *testing.T, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), 3)
