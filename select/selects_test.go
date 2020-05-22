7c13b0a30e99e41405d103cc44959f6da49566b3
SJ-KR <djtm99@naver.com>
2020-05-21 23:43:35 -0700

    update selects (configurable timeout)

--- select/selects_test.go
+++ select/selects_test.go
@@ -14,8 +14,8 @@ func TestRacer(t *testing.T) {
 
 		defer serverA.Close()
 		defer serverB.Close()
-
-		_, err := Racer(serverA.URL, serverB.URL)
+		duration := time.Second * 10
+		_, err := Racer(serverA.URL, serverB.URL, duration)
 		if err == nil {
 			t.Errorf("expected an error but didn't get one")
 		}
