1e807106c17876553b4f660037f718e5a2c14787
SJ-KR <djtm99@naver.com>
2020-05-23 07:25:01 -0700

    update numeral (added a struct array)

--- numeral/numeral.go
+++ numeral/numeral.go
@@ -2,28 +2,27 @@ package numeral
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumeral = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(x int) string {
 	var result strings.Builder
 
-	for x > 0 {
-		switch {
-		case x > 9:
-			x -= 10
-			result.WriteString("X")
-		case x > 8:
-			x -= 9
-			result.WriteString("IX")
-		case x > 4:
-			x -= 5
-			result.WriteString("V")
-		case x > 3:
-			x -= 4
-			result.WriteString("IV")
-		default:
-			x -= 1
-			result.WriteString("I")
+	for _, numeral := range allRomanNumeral {
+		for x >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			x -= numeral.Value
 		}
-
 	}
 
 	return result.String()
