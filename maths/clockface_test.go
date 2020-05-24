d1052ad02803e0e4c239a062838e87133a6f784d
SJ-KR <djtm99@naver.com>
2020-05-24 01:24:13 -0700

    update clockface

--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -1,7 +1,7 @@
-package clockface_test
+package clockface
 
 import (
-	clockface "learn-go/maths"
+	"math"
 	"testing"
 	"time"
 )
@@ -9,10 +9,20 @@ import (
 func TestSecondHandAtMidnight(t *testing.T) {
 	times := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(times)
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(times)
 
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+func TestSecondsInRadian(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+
+	want := math.Pi
+	got := SecondsInRadian(thirtySeconds)
+
+	if got != want {
+		t.Fatalf("want %v radians, but got %v", want, got)
 	}
 }
