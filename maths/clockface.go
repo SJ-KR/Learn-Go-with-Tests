ae18c3cba0bb63f74bd3ec4ffdda83169aec2a36
SJ-KR <djtm99@naver.com>
2020-05-24 02:45:06 -0700

    update clockface (new struct : Circle , Line (convert to float64))

--- maths/clockface.go
+++ maths/clockface.go
@@ -46,27 +46,26 @@ func SecondHandPoint(t time.Time) Point {
 
 type Svg struct {
 	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-	ViewBox string   `xml:"viewBox,attr"`
-	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
+
+	Xmlns   string `xml:"xmlns,attr"`
+	Width   string `xml:"width,attr"`
+	Height  string `xml:"height,attr"`
+	ViewBox string `xml:"viewBox,attr"`
+	Version string `xml:"version,attr"`
+
+	Circle Circle `xml:"circle"`
+	Line   []Line `xml:"line"`
+}
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
 }
 
 const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
