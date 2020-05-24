efebf3beed2a82e50721d48515ff945a969c6823
SJ-KR <djtm99@naver.com>
2020-05-24 02:34:32 -0700

    update clockface (using xml type)

--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -66,6 +68,25 @@ func TestSecondHandVector(t *testing.T) {
 		})
 
 	}
+}
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+	SVGWriter(&b, tm)
+
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150"
+	y2 := "60"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
 }
 func roughlyEqualFloat64(a, b float64) bool {
 	const Threshold = 1e-7
